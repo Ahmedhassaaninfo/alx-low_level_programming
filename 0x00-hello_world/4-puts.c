cedf189acc270756e25a4d1cdefd00c67cd2a247
“Joshua” <ahmedjoshua97@gmail.com>
2022-09-14 22:44:00 -0700

    put code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+*Description: using the main funcion
+*This program prints "Programming is like building a mutilingual puzzle
+*Retun: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
